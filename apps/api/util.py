ad1d46bbec2845c2d920c45fc19ab118a40b99bd
nasirali1 <nasir.ali08@gmail.com>
2022-07-25 12:44:06 -0500

    restored method

--- apps/api/util.py
+++ apps/api/util.py
@@ -4,3 +4,13 @@ from datetime import datetime
 def time_8601(time=datetime.now()) -> str:
     return time.astimezone().isoformat()
 
+def get_class_object(class_path:str):
+    from importlib import import_module
+
+    module_path, class_name = class_path.rsplit('.', 1)
+    module = import_module(module_path)
+
+    return getattr(module, class_name)
+
+obj = get_class_object("apps.learning_models.RandomSampling.RandomSampling")
+result = obj().run()
